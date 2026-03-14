e6ce707f7e2ffcc82c0e03be5293135a9367e053
david-rai <davidkhaling123@gmail.com>
2026-03-15 00:21:43 +0545

    Feat:Left-right divs added

--- src/components/Hackathons.jsx
+++ src/components/Hackathons.jsx
@@ -3,7 +3,7 @@ import { hackathonList } from "../constants/hackathons";
 
 const Hackathons = () => {
   return (
-    <section className="w-full pt-16">
+    <section className="w-full py-16">
       {/* Top bar */}
       <header className="flex items-center justify-center flex-col pb-10">
         <h1 className="bg-text text-bg p-2 rounded-md mb-5">Hackathons</h1>
@@ -15,46 +15,59 @@ const Hackathons = () => {
       </header>
 
       {/* All the projects Cards */}
-      <div className="flex flex-wrap items-center justify-between gap-5">
+      <div className="flex flex-col items-center justify-between gap-5">
         {hackathonList.map((p, i) => (
-          <HackathonChild p={p} key={i} />
+          <HackathonChild p={p} key={i} index={i + 1} />
         ))}
       </div>
     </section>
   );
 };
 
-const HackathonChild = ({ p }) => {
+const HackathonChild = ({ p, index }) => {
   const { name, details, country, links, image } = p;
+  const position = index % 2 === 0 ? "right" : "left";
+  console.log(position);
 
   return (
-    <div className="border border-border rounded-md w-[48%] min-h-[350px] p-2">
-      {/* Top Image banner */}
-      <div className="h-[60%] w-full">
-        <img src={image} alt="image" className="h-full w-full object-cover rounded-md"/>
-      </div>
+    <section
+      className="w-full flex"
+      style={{
+        justifyContent: position === "left" ? "flex-start" : "flex-end",
+      }}
+    >
+      <div className="border border-border rounded-md w-[48%] min-h-[350px] p-2">
+        {/* Top Image banner */}
+        <div className="h-[60%] w-full">
+          <img
+            src={image}
+            alt="image"
+            className="h-full w-full object-cover rounded-md"
+          />
+        </div>
 
-      <h1 className="text-text text-[16px pt-2">{name}</h1>
-      <h2>{country}</h2>
-      <p className="text-xs">{details}</p>
+        <h1 className="text-text text-[16px pt-2">{name}</h1>
+        <h2>{country}</h2>
+        <p className="text-xs">{details}</p>
 
-      {/* Links */}
-      <div className="flex gap-2 pt-3">
-        {links.length > 0 &&
-          links.map((l, i) => {
-            return (
-              <a
-                key={i}
-                href={l.src}
-                className="text-bg bg-text text-xs p-1 rounded-md font-medium"
-                target="_blank"
-              >
-                {l.type}
-              </a>
-            );
-          })}
+        {/* Links */}
+        <div className="flex gap-2 pt-3">
+          {links.length > 0 &&
+            links.map((l, i) => {
+              return (
+                <a
+                  key={i}
+                  href={l.src}
+                  className="text-bg bg-text text-xs p-1 rounded-md font-medium"
+                  target="_blank"
+                >
+                  {l.type}
+                </a>
+              );
+            })}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
