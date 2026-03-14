9e6632ba13f94ea418a463ce463e17a38d21a626
david-rai <davidkhaling123@gmail.com>
2026-03-15 02:59:48 +0545

    Style:Finally responsive design

--- src/components/TopHeading.jsx
+++ src/components/TopHeading.jsx
@@ -6,8 +6,8 @@ const TopHeading = () => {
   return (
     <div className="w-full pt-16 md:pt-25 flex items-center justify-between pb-10">
       <div className="left w-full">
-        <h1 className="md:text-6xl font-semibold">Hi,I`m David</h1>
-        <h2 className="md:text-xl text-md text-text-secondary py-2">
+        <h1 className=" text-3xl md:text-6xl font-semibold">Hi,I`m David</h1>
+        <h2 className="md:text-xl text-md text-text-secondary py-2 text-left">
           Building FullStack Applications that sloves.
         </h2>
         <h3 className="text-[14px] hover:underline cursor-pointer text-text font-medium flex items-center justify-start gap-2">
@@ -15,7 +15,7 @@ const TopHeading = () => {
           davidkhaling123@gmail.com
         </h3>
       </div>
-      <div className="right h-full w-[30%]">
+      <div className="right h-full w-[40%] md:w-[30%]">
         <img src={mainImage} className="h-full w-full" />
       </div>
     </div>
